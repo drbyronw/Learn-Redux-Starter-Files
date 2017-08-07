24292f0918ecf962757408d9357a3e15ebb839b3
Byron J. Williams <byron.w@ieee.org>
2017-08-07 18:28:03 -0500

    hello world - working app - plus linter changes

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from "react";
+import ReactDOM from "react-dom";
+// import css
+import css from "./styles/style.styl";
+
+ReactDOM.render(<p>Hello Byron</p>, document.getElementById("root"));
